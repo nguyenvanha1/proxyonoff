3680e27062fb1329992636411d1b0fa7cafc3d1a
nguyenvanha1 <nguyenvanhazing@gmail.com>
2024-12-20 08:33:43 +0700

    Add files via upload

---
+++ noproxy/background.js
@@ -0,0 +1,12 @@
+ chrome.proxy.settings.set({
+        value: {
+            mode: "system"
+        },
+        scope: "regular"
+    }, () => {
+        if (chrome.runtime.lastError) {
+            console.error("Error clearing proxy:", chrome.runtime.lastError.message);
+        } else {
+            console.log"Proxy settings cleared.");
+        }
+    }); 
